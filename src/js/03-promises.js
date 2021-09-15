4a1c742f5e21a66c7eaa2272d3da9bb0a5b24d79
Artur Kraśniewski <artur.krasniewski1@gmail.com>
2021-09-15 23:46:39 +0200

    małe kroczki

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,16 @@
+//make life easier
+const qs = selector => document.querySelector(selector);
+const qsa = selector => document.querySelectorAll(selector);
+const log = something => console.log(something);
+const cElem = elem => document.createElement(elem);
+
+const inputDelay = qs('input[name="delay"]');
+const inputStep = qs('input[name="step"]');
+const inputAmount = qs('input[name="amount"]');
+const btnsubmit = qs('button[type="submit"]');
+
+let delay = null;
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -5,4 +18,19 @@ function createPromise(position, delay) {
   } else {
     // Reject
   }
+}
+
+for (let i = 1; i < inputAmount.value; i++) {
+  if ((i = 1)) {
+    delay = inputDelay.value;
+  } else {
+    delay = inputStep.Value;
+  }
+  createPromise(i, delay)
+    .then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
 }

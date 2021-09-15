2b01a29c701b2321fef901a5eb41d2e26f3bed36
Artur Kraśniewski <artur.krasniewski1@gmail.com>
2021-09-15 21:29:53 +0200

    task 01 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const qs = selector => document.querySelector(selector);
+const qsa = selector => document.querySelectorAll(selector);
+const log = something => console.log(something);
+const cElem = elem => document.createElement(elem);
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const body = qs('body');
+const btnStart = qs('button[data-start]');
+const btnStop = qs('button[data-stop]');
+let timerId = null;
+
+log(body);
+log(btnStart);
+log(btnStop);
+
+btnStop.setAttribute('disabled', true);
+btnStart.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    let newColor = getRandomHexColor();
+    body.style.backgroundColor = newColor;
+  }, 1000);
+  btnStart.setAttribute('disabled', true);
+  btnStop.removeAttribute('disabled');
+});
+
+btnStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  btnStart.removeAttribute('disabled');
+  btnStop.setAttribute('disabled', true);
+});
